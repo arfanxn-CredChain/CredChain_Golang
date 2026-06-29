7c28e31bd201305fe73e16b2df2d479f6bf84782
arfanxn <arfan2173@gmail.com>
2026-06-29 23:18:17 +0700

    feat: register MetaService and MetaHandler in FX DI

--- cmd/server.go
+++ cmd/server.go
@@ -6,6 +6,7 @@ import (
 	"CredChain_Golang/domain"
 	"CredChain_Golang/feature/auth"
 	"CredChain_Golang/feature/credential"
+	"CredChain_Golang/feature/meta"
 	"CredChain_Golang/feature/overview"
 	"CredChain_Golang/feature/user"
 	"CredChain_Golang/infrastructure/ai/pyai"
@@ -64,6 +65,8 @@ var serverCmd = &cobra.Command{
 				overview.NewGormOverviewRepository,
 				overview.NewOverviewService,
 				overview.NewOverviewHandler,
+				meta.NewMetaService,
+				meta.NewMetaHandler,
 				infraJobs.NewCredentialExtractWorker,
 				infraJobs.NewRiverClient,
 				func(db *gorm.DB) domain.UnitOfWork {
